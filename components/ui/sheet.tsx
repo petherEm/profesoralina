e05754225238f2568cc62a270c270db9b0402c8d
Piotr Maciejewski <pether.maciejewski@gmail.com>
2023-10-01 18:29:07 +0200

    updates shadcn radix

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -13,8 +13,11 @@ const SheetTrigger = SheetPrimitive.Trigger;
 
 const SheetClose = SheetPrimitive.Close;
 
-const SheetPortal = ({ ...props }: SheetPrimitive.DialogPortalProps) => (
-  <SheetPrimitive.Portal />
+const SheetPortal = ({
+  className,
+  ...props
+}: SheetPrimitive.DialogPortalProps) => (
+  <SheetPrimitive.Portal className={cn(className)} {...props} />
 );
 SheetPortal.displayName = SheetPrimitive.Portal.displayName;
 
